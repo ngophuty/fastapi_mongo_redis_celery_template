a934a46d34e0c0ebe3a254e913d1ce5465a86594
tynp <ngophuty233@gmail.com>
2022-12-22 09:27:48 +0000

    fix scripts

--- scripts/initialize_project.sh
+++ scripts/initialize_project.sh
@@ -1,8 +1,10 @@
-create_venv="python3 -m venv venv"
+create_venv="python3.10 -m venv venv"
+active_venv="source venv/bin/activate"
 install_poetry="pip install poetry"
 install_package="poetry install"
 
 eval $create_venv
+eval $active_venv
 eval $install_poetry
 eval $install_package
 
